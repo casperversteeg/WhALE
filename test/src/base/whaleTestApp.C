3391d699b2f7ce24b0421ec17b1b917e687ea473
casperversteeg <casper.versteeg@outlook.com>
2019-06-21 08:52:12 -0400

    Initial files

---
+++ test/src/base/whaleTestApp.C
@@ -0,0 +1,63 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "whaleTestApp.h"
+#include "whaleApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+#include "ModulesApp.h"
+
+template <>
+InputParameters
+validParams<whaleTestApp>()
+{
+  InputParameters params = validParams<whaleApp>();
+  return params;
+}
+
+whaleTestApp::whaleTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  whaleTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+whaleTestApp::~whaleTestApp() {}
+
+void
+whaleTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  whaleApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"whaleTestApp"});
+    Registry::registerActionsTo(af, {"whaleTestApp"});
+  }
+}
+
+void
+whaleTestApp::registerApps()
+{
+  registerApp(whaleApp);
+  registerApp(whaleTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+whaleTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  whaleTestApp::registerAll(f, af, s);
+}
+extern "C" void
+whaleTestApp__registerApps()
+{
+  whaleTestApp::registerApps();
+}
